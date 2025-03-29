71308336ef2b70e9bed857266265ece654ef0876
Ulderico <s307530@studenti.polito.it>
2025-03-29 17:39:17 +0100

    caricamento esercizio

--- Exercise2/main.cpp
+++ Exercise2/main.cpp
@@ -1,4 +1,52 @@
-int main()
+#include <iostream>
+#include<fstream>
+#include<iomanip>
+
+using namespace std;
+double traslazione(double x)
 {
+	double y= x*0.75-1.75;
+	return y;
+}
+	
+	
+
+int main()
+{	ifstream file("data.txt");
+	
+	 if(file.fail())
+    {
+        cerr << "File not found" << endl;
+		
+        return 1;
+    }
+	string filename= "result.txt";
+	ofstream ofs(filename);
+	if (! ofs.is_open())
+	{
+		cerr<<"errore"<<endl;
+		return 1;
+	}
+	
+	double val;
+	double sum=0;
+	double w=1.0;
+	double mean;
+	double new_val;
+	
+	ofs<<"#N Mean"<<endl;
+	while(file>>val) 
+	{
+		new_val=traslazione(val);
+		sum+= new_val;
+		mean= sum/w;
+		w++;
+		ofs<<scientific<<setprecision(16)<<mean<<endl;
+		
+	}
+	file.close();
+	
+	
+
     return 0;
 }
